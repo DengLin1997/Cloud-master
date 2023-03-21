a89fde238256ca2101482c493357d3b1bbaaeb61
denglin <2466970160@qq.com>
2023-03-21 10:12:44 +0800

    测试

---
+++ ruoyi-modules/ruo-yi-service-eims/src/test/java/com/ruoyi/eims/test01.java
@@ -0,0 +1,16 @@
+package com.ruoyi.eims;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class test01 {
+
+    @Test
+    public void test01() {
+        System.out.println("test01");
+    }
+}
