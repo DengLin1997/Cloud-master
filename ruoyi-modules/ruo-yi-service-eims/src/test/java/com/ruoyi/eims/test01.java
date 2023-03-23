ff7d2dcaddc3744b605e1e807636b2d3f7f077ca
denglin <2466970160@qq.com>
2023-03-23 09:36:15 +0800

    测试

--- ruoyi-modules/ruo-yi-service-eims/src/test/java/com/ruoyi/eims/test01.java
+++ ruoyi-modules/ruo-yi-service-eims/src/test/java/com/ruoyi/eims/test01.java
@@ -1,16 +1,30 @@
 package com.ruoyi.eims;
 
+import com.ruoyi.eims.domain.SalesOrder;
+import com.ruoyi.eims.mapper.SalesOrderMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class test01 {
 
+    @Autowired
+    private SalesOrderMapper salesOrderMapper;
+
     @Test
     public void test01() {
         System.out.println("test01");
+    }
+
+    @Test
+    public void test02(){
+        List<SalesOrder> salesOrders = salesOrderMapper.selectSalesOrderList(null);
+        System.out.println(salesOrders);
     }
 }
